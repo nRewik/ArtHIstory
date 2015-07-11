c8ae02c288eef8dfe105710bf93d14335269b4c5
nRewik <nrewik@outlook.com>
2015-07-11 12:31:08 +0700

    add font & install cocoapods

---
+++ ArtHistory/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// BubbleTransition
+#define COCOAPODS_POD_AVAILABLE_BubbleTransition
+#define COCOAPODS_VERSION_MAJOR_BubbleTransition 0
+#define COCOAPODS_VERSION_MINOR_BubbleTransition 2
+#define COCOAPODS_VERSION_PATCH_BubbleTransition 0
+
+// ChameleonFramework/Default
+#define COCOAPODS_POD_AVAILABLE_ChameleonFramework_Default
+#define COCOAPODS_VERSION_MAJOR_ChameleonFramework_Default 1
+#define COCOAPODS_VERSION_MINOR_ChameleonFramework_Default 2
+#define COCOAPODS_VERSION_PATCH_ChameleonFramework_Default 1
+
+// ChameleonFramework/Swift
+#define COCOAPODS_POD_AVAILABLE_ChameleonFramework_Swift
+#define COCOAPODS_VERSION_MAJOR_ChameleonFramework_Swift 1
+#define COCOAPODS_VERSION_MINOR_ChameleonFramework_Swift 2
+#define COCOAPODS_VERSION_PATCH_ChameleonFramework_Swift 1
+
