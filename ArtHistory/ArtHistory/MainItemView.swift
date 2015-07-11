d395b31ad700b2740341ed1fea2962b1d19a4220
nRewik <nrewik@outlook.com>
2015-07-11 23:36:15 +0700

    add MainVC, add LessionVC

---
+++ ArtHistory/ArtHistory/MainItemView.swift
@@ -0,0 +1,70 @@
+//
+//  MainItemView.swift
+//  ArtHistory
+//
+//  Created by Nutchaphon Rewik on 7/11/15.
+//  Copyright (c) 2015 Nutchaphon Rewik. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class MainItemView: UIView {
+    
+    var action: ( () -> Void )?
+    var circleCenter: CGPoint{
+        return button.center
+    }
+    
+    @IBInspectable var text:String?{
+        didSet{
+            label?.text = text
+        }
+    }
+    @IBInspectable var image:UIImage?{
+        didSet{
+            button.setImage(image, forState: .Normal)
+        }
+    }
+    
+    @IBAction func buttonPressed() {
+        action?()
+    }
+    
+    @IBOutlet weak private var topView: UIView!
+    @IBOutlet weak private var button: UIButton!
+    @IBOutlet weak private var label: UILabel!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layoutIfNeeded()
+        
+        button.layer.cornerRadius = button.frame.width / 2.0
+        button.clipsToBounds = true
+    }
+    
+    // MARK: Custom View Initilization
+    // Our custom view from the XIB file
+    var view: UIView!
+    
+    func xibSetup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+        addSubview(view)
+        //// set up view here
+    }
+    func loadViewFromNib() -> UIView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "MainItemView", bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        return view
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
+    }
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetup()
+    }
+}
--- ArtHistory/Pods/Target Support Files/Pods/Pods-environment.h
+++ ArtHistory/Pods/Target Support Files/Pods/Pods-environment.h
@@ -24,3 +24,9 @@
 #define COCOAPODS_VERSION_MINOR_ChameleonFramework_Swift 2
 #define COCOAPODS_VERSION_PATCH_ChameleonFramework_Swift 1
 
+// FontAwesome.swift
+#define COCOAPODS_POD_AVAILABLE_FontAwesome_swift
+#define COCOAPODS_VERSION_MAJOR_FontAwesome_swift 0
+#define COCOAPODS_VERSION_MINOR_FontAwesome_swift 4
+#define COCOAPODS_VERSION_PATCH_FontAwesome_swift 3
+
