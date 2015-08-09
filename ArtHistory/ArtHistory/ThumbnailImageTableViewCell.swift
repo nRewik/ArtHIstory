ed0b04717a66adeb13f975e279992882edd2aa0b
nRewik <nrewik@outlook.com>
2015-08-09 16:30:54 +0700

    change lesson's image cell

--- ArtHistory/ArtHistory/ThumbnailImageTableViewCell.swift
+++ ArtHistory/ArtHistory/ThumbnailImageTableViewCell.swift
@@ -22,12 +22,20 @@ class ThumbnailImageTableViewCell: UITableViewCell {
         }
     }
     
+    @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        contentView.layer.shadowColor = UIColor.blackColor().CGColor
+        contentView.layer.shadowOffset = CGSize(width: 0.0, height: 0.5)
+        contentView.layer.shadowRadius = 1.0
+        contentView.layer.shadowOpacity = 0.2
+        
+        cardView.layer.cornerRadius = 2.5
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
